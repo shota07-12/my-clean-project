c2c5b64ae093bb783c763f3143bf76f463c08f1a
shota <ya07masho12@gmail.com>
2025-03-23 19:21:37 +0900

    fix: add files binding for Fortify

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,17 +11,16 @@ class AppServiceProvider extends ServiceProvider
      * Register any application services.
      */
     public function register(): void
-    {
-        //
+    { {
+            $this->app->bind('files', function () {
+                return new Filesystem;
+            });
+        }
     }
 
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        $this->app->bind('files', function () {
-            return new Filesystem;
-        });
-    }
+    //
 }
